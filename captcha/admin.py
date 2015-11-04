b8732d329722c70f1b05dc0c6a15b3f7be68b94a
Lohit <lohitaksh14059@iiitd.ac.in>
2015-11-05 03:11:01 +0530

    Changed Case db

--- captcha/admin.py
+++ captcha/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from captcha.models import *
 
 # Register your models here.
+
+Models = [Case,Captcha,Run]
+
+admin.site.register(Models)
--- captcha/models.py
+++ captcha/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Case(models.Model):
-	allowed = models.IntegerField(default=0)
-	minimum = models.IntegerField(default=0)
+	n = models.IntegerField(default=0)		#number of objects
+	num = models.IntegerField(default=0)    #case number
 
 class Captcha(models.Model):
 	image = models.CharField(max_length=10)
