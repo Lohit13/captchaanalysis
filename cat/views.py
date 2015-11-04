08e671b05c02911606f412d8e8973e43c669ae43
Kushagra Singh <singh.kushagra.1996@gmail.com>
2015-11-05 01:58:21 +0530

    Giving unique hash to session

--- cat/views.py
+++ cat/views.py
@@ -3,14 +3,35 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.core.context_processors import csrf
 
+
+import hashlib
+import datetime
+
 # Create your views here.
 
 def index(request):
+	args = {}
+
+	try:
+		hashCode = request.session["userHash"]
+	except:
+
+		# assign new hash
+
+		hasher = hashlib.sha1()
+		hasher.update(str(datetime.datetime.now()))
+		request.session["userHash"] = hasher.hexdigest()
+		
+
 	if request.method == "POST":
-		print request.POST['name']
-		return HttpResponse("sup")
-	else:
-		args = {}
-		args.update(csrf(request))
-		return render_to_response('index.html',args)
+		givenName = request.POST['name']
+		
+		if givenName == "":
+			args["nameError"] = True
+		
+		else:
+			request.session["name"] = request.POST['name']
+
+	args.update(csrf(request))
+	return render_to_response('index.html',args)
 
