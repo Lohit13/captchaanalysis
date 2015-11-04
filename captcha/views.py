f8ac9603474886bf0b61ab96456b197c07d64970
Kushagra Singh <singh.kushagra.1996@gmail.com>
2015-11-05 02:15:43 +0530

    Added image url

--- captcha/views.py
+++ captcha/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from django.core.context_processors import csrf
+
+
+import hashlib
+import datetime
 
 # Create your views here.
 
-#def image(request):
+def image(request):
+	return render_to_response("captcha.html")
--- cat/urls.py
+++ cat/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from captcha.views import *
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'cat.views.index', name='home'),
+    url(r'^captcha/', 'captcha.views.image', name='image'),
 
 
    	
